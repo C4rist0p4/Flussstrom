f4055873a2ce511fe0fb9ecfc68f8665774ef112
C4rist0p4 <Christoph_S@posteo.de>
2020-03-06 15:37:14 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/CreateUser.java
@@ -0,0 +1,63 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class CreateUser extends AppCompatActivity {
+    private FirebaseAuth mAuth;
+
+    private TextView emailET;
+    private TextView passwordET;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_user);
+
+        mAuth = FirebaseAuth.getInstance();
+        emailET = findViewById(R.id.emailET);
+        passwordET = findViewById(R.id.Passwor1ET);
+    }
+
+    public void createUser(View view) {
+        String email = emailET.getText().toString();
+        String password = passwordET.getText().toString();
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d("success", "createUserWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w("failure", "createUserWithEmail:failure", task.getException());
+                            Toast.makeText(CreateUser.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        Log.i("User", "is: " + currentUser);
+    }
+}
---
+++ app/src/main/java/com/example/myapplication/SqlInterface.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SqlInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sql_interface);
+    }
+}
