38a3638b9e4b3fb82724b58926570ea58d7078f8
C4rist0p4 <Christoph_S@posteo.de>
2020-03-13 15:11:39 +0100

    second commit

--- app/src/main/java/com/example/myapplication/SqlInterface.java
+++ app/src/main/java/com/example/myapplication/SqlInterface.java
@@ -1,14 +1,89 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.functions.FirebaseFunctions;
+import com.google.firebase.functions.FirebaseFunctionsException;
+import com.google.firebase.functions.HttpsCallableResult;
+
+import java.util.HashMap;
+import java.util.Objects;
+
 
 public class SqlInterface extends AppCompatActivity {
+    private FirebaseFunctions mFunctions;
+    private TextView dataTv;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sql_interface);
+        dataTv = findViewById(R.id.dataEt);
+        mFunctions = FirebaseFunctions.getInstance();
+    }
+
+    private Task<String> addMessage() {
+
+        return mFunctions
+                .getHttpsCallable("getMeldung")
+                .call()
+                .continueWith(new Continuation<HttpsCallableResult, String>() {
+
+                    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+                    public String then(@NonNull Task<HttpsCallableResult> task) throws Exception {
+                        HashMap<String, String> result;
+                        try {
+                            result = (HashMap<String, String>) Objects.requireNonNull(task.getResult()).getData();
+                            assert result != null;
+                            return result.toString();
+                        } catch (Exception e) {
+                            throw new Exception(e);
+                        }
+                    }
+                });
+    }
+
+    public void getSqlQuery(View view) {
+        addMessage()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Exception e = task.getException();
+                            if (e instanceof FirebaseFunctionsException) {
+                                FirebaseFunctionsException ffe = (FirebaseFunctionsException) e;
+                                FirebaseFunctionsException.Code code = ffe.getCode();
+                                Object details = ffe.getDetails();
+                            }
+                                // [START_EXCLUDE]
+                                Log.w("addMessage:onFailure", e);
+                                showSnackbar("An error occurred.");
+                                return;
+                                // [END_EXCLUDE]
+                            }
+                            // [START_EXCLUDE]
+                            String result = task.getResult();
+
+                            dataTv.setText(result);
+                            // [END_EXCLUDE]
+                        }
+                });
+    }
+
+    private void showSnackbar(String message) {
+        Snackbar.make(findViewById(android.R.id.content), message, Snackbar.LENGTH_SHORT).show();
     }
 }
